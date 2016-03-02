d754e56a65dcdea199e1ddc93b4868ae82953f03
wangyuanhsiang <ywang91@studentmail.nait.ca>
2016-03-02 07:25:17 -0700

    Record Helper - Shawn 2016/03/02

---
+++ UWPApp -record helper/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace UWPApp__record_helper
+{
+    public class Class1
+    {
+        public async void recordhelper(string e)
+        {
+            FileSavePicker picker = new FileSavePicker();
+            picker.FileTypeChoices.Add("file style", new string[] { ".txt" });
+            picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            picker.SuggestedFileName = "test.txt";
+            StorageFile file = await picker.PickSaveFileAsync();
+            if (file != null)
+            {
+                //byte[] data = new byte[200];
+                //Random rand = new Random();
+                //rand.NextBytes(data);
+                //await FileIO.WriteBytesAsync(file, data);
+
+                await FileIO.WriteTextAsync(file, e);
+            }
+        }
+    }
+}
---
+++ UWPApp -record helper/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPApp__record_helper
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        string a;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 c1 = new Class1();
+            a = tbValue.Text;
+            c1.recordhelper(a);
+        }
+    }
+}
